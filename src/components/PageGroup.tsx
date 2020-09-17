c8122e67f436bb3b7362029b6e30a3bbdc39a6cd
niconiahi <nicolas.accetta@gmail.com>
2020-09-17 09:41:29 -0300

    fix: Improve semantics

--- src/components/PageGroup.tsx
+++ src/components/PageGroup.tsx
@@ -3,7 +3,7 @@ import { Flex, FlexProps, Box } from "@chakra-ui/core";
 
 const PageGroup: FC<FlexProps> = ({ children, ...flexProps }) => (
   <Box aria-label="pagination navigation" as="nav">
-    <Flex {...flexProps} as="ul">
+    <Flex {...flexProps} as="ol">
       {children}
     </Flex>
   </Box>
