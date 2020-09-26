4d7baa3d849fd558247c38d186b60bddc1c1ad4d
niconiahi <nicolas.accetta@gmail.com>
2020-09-25 23:08:00 -0300

    test

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-import typescript from '@rollup/plugin-typescript'
+import typescript from "rollup-plugin-typescript2";
 
 const PACKAGE_NAME = "chakra-paginator";
 const ENTRY_FILE = "./src/index.ts";
@@ -11,11 +11,31 @@ const OUTPUT_DIR = "./dist";
 export default [
   {
     input: ENTRY_FILE,
-    output: {
-      file: `${OUTPUT_DIR}/${PACKAGE_NAME}.js`,
-      format: "cjs",
-      name: PACKAGE_NAME,
-    },
-    plugins: [resolve(), babel(), commonjs(), terser(), typescript()],
+    output: [
+      {
+        file: `${OUTPUT_DIR}/${PACKAGE_NAME}.js`,
+        format: "cjs",
+        exports: "named",
+        name: PACKAGE_NAME,
+        sourcemap: true,
+      },
+      {
+        file: `${OUTPUT_DIR}/${PACKAGE_NAME}.js`,
+        format: "es",
+        exports: "named",
+        name: PACKAGE_NAME,
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        rollupCommonJSResolveHack: false,
+        clean: true,
+      }),
+      resolve(),
+      babel(),
+      commonjs(),
+      terser(),
+    ],
   },
 ];
