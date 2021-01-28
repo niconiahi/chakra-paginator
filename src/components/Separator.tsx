38b37a90d67f7e0acafcbcf1f3658315bde4a270
niconiahi <nicolas.accetta@gmail.com>
2021-01-28 14:13:21 -0300

    fix: Improve html structure of pages

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useCallback, useContext, useMemo } from "react";
-import { Button, ButtonProps, Icon } from "@chakra-ui/react";
+import { Button, Flex, ButtonProps, Icon } from "@chakra-ui/react";
 
 // components
 import FiMoreHorizontal from "./FiMoreHorizontal";
@@ -47,51 +47,52 @@ export const Separator: FC<SeparatorProps> = ({
   }, [pageToJump, isDisabled, setCurrentPage]);
 
   return (
-    <Button
-      align="center"
-      aria-label={`Jump pages ${jumpingDirection}`}
-      as="li"
-      bg="transparent"
-      cursor={isDisabled ? "not-allowed" : "pointer"}
-      justify="center"
-      minW="auto"
-      onClick={handleJumpClick}
-      pointerEvents={isDisabled ? "none" : "auto"}
-      pos="relative"
-      px={1}
-      sx={{
-        _hover: {
-          ".call-to-action": {
-            opacity: 1,
+    <Flex as="li">
+      <Button
+        align="center"
+        aria-label={`Jump pages ${jumpingDirection}`}
+        bg="transparent"
+        cursor="pointer"
+        justify="center"
+        minW="auto"
+        onClick={handleJumpClick}
+        pointerEvents={isDisabled ? "none" : "auto"}
+        pos="relative"
+        px={1}
+        sx={{
+          _hover: {
+            ".call-to-action": {
+              opacity: 1,
+            },
           },
-        },
-      }}
-      {...(isDisabled ? { "aria-disabled": true } : {})}
-      {...separatorStyles}
-    >
-      <Icon
-        as={separatorIcon ?? FiMoreHorizontal}
-        bg="inherit"
-        color="inherit"
-        h={4}
-        w={4}
-      />
-      <Icon
-        as={hoverIcon}
-        bg="inherit"
-        bottom={0}
-        className="call-to-action"
-        color="inherit"
-        h={4}
-        left={0}
-        m="auto"
-        opacity={0}
-        pos="absolute"
-        right={0}
-        top={0}
-        transition="all  cubic-bezier(0.4, 1, 0.9, 0.6) 0.3s"
-        w={4}
-      />
-    </Button>
+        }}
+        {...(isDisabled ? { "aria-disabled": true } : {})}
+        {...separatorStyles}
+      >
+        <Icon
+          as={separatorIcon ?? FiMoreHorizontal}
+          bg="inherit"
+          color="inherit"
+          h={4}
+          w={4}
+        />
+        <Icon
+          as={hoverIcon}
+          bg="inherit"
+          bottom={0}
+          className="call-to-action"
+          color="inherit"
+          h={4}
+          left={0}
+          m="auto"
+          opacity={0}
+          pos="absolute"
+          right={0}
+          top={0}
+          transition="all  cubic-bezier(0.4, 1, 0.9, 0.6) 0.3s"
+          w={4}
+        />
+      </Button>
+    </Flex>
   );
 };
