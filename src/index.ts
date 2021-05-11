7aaad80f0dd6abe6032eabea9ddfdb62a13ea707
niconiahi <nicolas.accetta@gmail.com>
2021-05-11 20:10:14 -0300

    fix: export use paginator hook

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { PageGroup } from "./components/PageGroup";
 export { Paginator } from "./components/Paginator";
 export { Previous } from "./components/Previous";
 export { Container } from "./components/Container";
+export { usePaginator } from "./lib/hooks/usePaginator";
